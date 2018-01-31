268900bf65c4b840520e86706ae4287a0f281dab
Joe Waine <joseph@comradeagency.com>
2018-01-31 14:45:34 -0800

    filtering

--- example-sites.php
+++ example-sites.php
@@ -24,21 +24,52 @@
         </div>
 
 
-    <div class="row col-md-12 mt50">
+
+<div class="row col-md-12">
+
+
+<!-- here it is -->
+<div class="button-group filter-button-group">
+
+
+
+       <?php while ( have_rows('categories_repeater') ) : the_row(); ?>
+
+
+<span class="button filter" data-filter="<?php the_sub_field('category-slug') ?>"><?php the_sub_field('category') ?> <i class="fa fa-chevron-right"></i></span>
+        <?php endwhile; ?>
+
+<!--
+  <span class="button filter" data-filter=".cat">cat <i class="fa fa-chevron-right"></i></span>
+  <span class="button filter" data-filter=".dog">dog <i class="fa fa-chevron-right"></i></span>
+  <span class="button filter" data-filter=".mouse">mouse <i class="fa fa-chevron-right"></i></span>
+ --></div>
+
+<!-- <select class="button-group filter-button-group">
+  <option data-filter="*">show all</option>
+  <option data-filter=".cat">cat</option>
+  <option data-filter=".dog">dog</option>
+  <option data-filter=".mouse">mouse</option>
+</select> -->
+
+
+</div>
+
+    <div class="row col-md-12 mt50 grid margin-left:20px;">
 
 
        <?php while ( have_rows('example_sites_repeater') ) : the_row(); ?>
 
 
-<div class="col-sm-12 col-md-4">
-  <div class="landing-panel l-grey">
+<div class="third element-item transition <?php the_sub_field('category-slug') ?>">
+  <div class="landing-panel l-grey" style="width:96%;margin: 0 auto 20px;">
 
 <!-- lilnks -->
   <div class="top-links"><?php the_sub_field('top_link_text') ?> <i class="fa fa-external-link" aria-hidden="true"></i></a></div>
 <!-- lilnks -->
      <div class="site-image">
       <a target="_blank" href="<?php the_sub_field('top_link_url') ?>">
-   <img src="<?php the_sub_field('example_image') ?>">
+   <!-- <img src="<?php the_sub_field('example_image') ?>"> -->
   <!-- <img src="https://i.imgur.com/IRJlsJs.png" alt="lightbulb"> -->
 
             <div class="site-text">
@@ -52,7 +83,7 @@
             </div>
   <div class="trending-tags">
 <a href=""><?php the_sub_field('trending_tags_text') ?></a>
-
+<!--<?php the_sub_field('category') ?>-->
   </div>
 
 </div></div>
@@ -68,4 +99,12 @@
 
       </div>
     </div>
+
+
+
+
+
+
+
+
 </header>
---
+++ filtering-panel.php
@@ -0,0 +1,129 @@
+  <div id="content" class="main">
+    <div class="container">
+
+
+
+
+
+<div class="big-demo go-wide" data-js="filtering-demo">
+  <div class="filter-button-group button-group js-radio-button-group">
+    <button class="button is-checked" data-filter="*">show all</button>
+    <button class="button" data-filter=".metal">metal</button>
+    <button class="button" data-filter=".transition">transition</button>
+    <button class="button" data-filter=".alkali, .alkaline-earth">alkali &amp; alkaline-earth</button>
+
+  </div>
+
+  <div class="grid" style="position: relative; height: 224px;">
+      <div class="element-item transition metal " data-category="transition" style="position: absolute; left: 0px; top: 0px;">
+        <h5 class="name">Mercury</h5>
+        <p class="symbol">Hg</p>
+        <p class="number">80</p>
+        <p class="weight">200.59</p>
+      </div>
+      <div class="element-item metalloid " data-category="metalloid" style="position: absolute; left: 110px; top: 0px;">
+        <h5 class="name">Tellurium</h5>
+        <p class="symbol">Te</p>
+        <p class="number">52</p>
+        <p class="weight">127.6</p>
+      </div>
+      <div class="element-item post-transition metal " data-category="post-transition" style="position: absolute; left: 220px; top: 0px;">
+        <h5 class="name">Bismuth</h5>
+        <p class="symbol">Bi</p>
+        <p class="number">83</p>
+        <p class="weight">208.980</p>
+      </div>
+      <div class="element-item post-transition metal " data-category="post-transition" style="position: absolute; left: 330px; top: 0px;">
+        <h5 class="name">Lead</h5>
+        <p class="symbol">Pb</p>
+        <p class="number">82</p>
+        <p class="weight">207.2</p>
+      </div>
+      <div class="element-item transition metal " data-category="transition" style="position: absolute; left: 440px; top: 0px;">
+        <h5 class="name">Gold</h5>
+        <p class="symbol">Au</p>
+        <p class="number">79</p>
+        <p class="weight">196.967</p>
+      </div>
+      <div class="element-item alkali metal " data-category="alkali" style="position: absolute; left: 550px; top: 0px;">
+        <h5 class="name">Potassium</h5>
+        <p class="symbol">K</p>
+        <p class="number">19</p>
+        <p class="weight">39.0983</p>
+      </div>
+      <div class="element-item alkali metal " data-category="alkali" style="position: absolute; left: 660px; top: 0px;">
+        <h5 class="name">Sodium</h5>
+        <p class="symbol">Na</p>
+        <p class="number">11</p>
+        <p class="weight">22.99</p>
+      </div>
+      <div class="element-item transition metal " data-category="transition" style="position: absolute; left: 770px; top: 0px;">
+        <h5 class="name">Cadmium</h5>
+        <p class="symbol">Cd</p>
+        <p class="number">48</p>
+        <p class="weight">112.411</p>
+      </div>
+      <div class="element-item alkaline-earth metal " data-category="alkaline-earth" style="position: absolute; left: 880px; top: 0px;">
+        <h5 class="name">Calcium</h5>
+        <p class="symbol">Ca</p>
+        <p class="number">20</p>
+        <p class="weight">40.078</p>
+      </div>
+      <div class="element-item transition metal " data-category="transition" style="position: absolute; left: 990px; top: 0px;">
+        <h5 class="name">Rhenium</h5>
+        <p class="symbol">Re</p>
+        <p class="number">75</p>
+        <p class="weight">186.207</p>
+      </div>
+      <div class="element-item post-transition metal " data-category="post-transition" style="position: absolute; left: 0px; top: 110px;">
+        <h5 class="name">Thallium</h5>
+        <p class="symbol">Tl</p>
+        <p class="number">81</p>
+        <p class="weight">204.383</p>
+      </div>
+      <div class="element-item metalloid " data-category="metalloid" style="position: absolute; left: 110px; top: 110px;">
+        <h5 class="name">Antimony</h5>
+        <p class="symbol">Sb</p>
+        <p class="number">51</p>
+        <p class="weight">121.76</p>
+      </div>
+      <div class="element-item transition metal " data-category="transition" style="position: absolute; left: 220px; top: 110px;">
+        <h5 class="name">Cobalt</h5>
+        <p class="symbol">Co</p>
+        <p class="number">27</p>
+        <p class="weight">58.933</p>
+      </div>
+      <div class="element-item lanthanoid metal inner-transition " data-category="lanthanoid" style="position: absolute; left: 330px; top: 110px;">
+        <h5 class="name">Ytterbium</h5>
+        <p class="symbol">Yb</p>
+        <p class="number">70</p>
+        <p class="weight">173.054</p>
+      </div>
+      <div class="element-item noble-gas nonmetal " data-category="noble-gas" style="position: absolute; left: 440px; top: 110px;">
+        <h5 class="name">Argon</h5>
+        <p class="symbol">Ar</p>
+        <p class="number">18</p>
+        <p class="weight">39.948</p>
+      </div>
+      <div class="element-item diatomic nonmetal " data-category="diatomic" style="position: absolute; left: 550px; top: 110px;">
+        <h5 class="name">Nitrogen</h5>
+        <p class="symbol">N</p>
+        <p class="number">7</p>
+        <p class="weight">14.007</p>
+      </div>
+      <div class="element-item actinoid metal inner-transition " data-category="actinoid" style="position: absolute; left: 660px; top: 110px;">
+        <h5 class="name">Uranium</h5>
+        <p class="symbol">U</p>
+        <p class="number">92</p>
+        <p class="weight">238.029</p>
+      </div>
+      <div class="element-item actinoid metal inner-transition " data-category="actinoid" style="position: absolute; left: 770px; top: 110px;">
+        <h5 class="name">Plutonium</h5>
+        <p class="symbol">Pu</p>
+        <p class="number">94</p>
+        <p class="weight">(244)</p>
+      </div>
+  </div>
+</div>
+    </div>
+  </div>
