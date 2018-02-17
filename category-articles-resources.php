c7f10b5597634d2de714b13d6e2e2b3d431f6d9c
Joe Waine <joseph@comradeagency.com>
2018-02-16 19:04:28 -0800

    adding fixes

--- category-articles-resources.php
+++ category-articles-resources.php
@@ -31,7 +31,7 @@
 <div class="row col-md-12">
 <!-- here it is -->
 <?php if( get_sub_field('filters_on_off') ){ $filtersOnOff = ''; }else{ $filtersOnOff = 'style="display:none"'; } ?>
-<div <?php echo $filtersOnOff ?>>
+<div <?php echo $filtersOnOff ?> style="display:none;">
   <div class='filter-by horizontal' style="color:#838383">Sort by type:&nbsp;&nbsp;&nbsp;&nbsp;</div>
 <div class="button-group filter-button-group float-left" <?php echo $filtersOnOff ?>>
 <span class="button filter" data-filter="*" style="color:#838383">all categories <i class="fa fa-chevron-right"></i></span>
